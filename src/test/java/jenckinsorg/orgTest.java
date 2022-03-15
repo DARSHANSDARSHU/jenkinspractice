3d8c02e14995eb26ff803fd244c33a04e9660722
Deepthi <Deepthi@DESKTOP-RMSCL2B>
2022-03-15 13:32:22 +0530

    jenkinspractice

---
+++ src/test/java/jenckinsorg/orgTest.java
@@ -0,0 +1,25 @@
+package jenckinsorg;
+
+import org.testng.annotations.Test;
+
+public class orgTest {
+	
+	
+@Test
+	
+	public void orgtest_1() {
+		
+		System.out.println("org_1 is created");
+		
+	}
+	
+	@Test
+	
+public void orgttest_2() {
+		
+		System.out.println("org_2 is created");
+		
+	}
+
+
+}
---
+++ src/test/java/jenkinpractice/contactTest.java
@@ -0,0 +1,23 @@
+package jenkinpractice;
+
+import org.testng.annotations.Test;
+
+public class contactTest {
+	
+	@Test
+	
+	public void contacttest_1() {
+		
+		System.out.println("cotact_1 is created");
+		
+	}
+	
+	@Test
+	
+public void contacttest_2() {
+		
+		System.out.println("cotact_2 is created");
+		
+	}
+
+}
